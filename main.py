ca04f48e8919a6ea15c2b17bb87996f6fea1cea5
hrj0903 <55598220+hrj0903@users.noreply.github.com>
2020-12-28 23:39:17 +0900

    add secret auction

---
+++ main.py
@@ -0,0 +1,25 @@
+from art import logo
+#HINT: You can call clear() to clear the output in the console.
+print(logo)
+
+who_winner_info = {}
+should_continue = True
+
+def highest_bidder(record):
+    max_score = 0
+    winner = ""
+    for name in record:
+        price = record[name]
+        if price > max_score:
+            winner = name
+            max_score = price
+    print(f"winner: {winner}, highest bid: ${max_score}")
+
+while should_continue:
+    name = input("Name: ")
+    bid = int(input("Bid: $"))
+    who_winner_info[name] = bid
+    more_bid = input("Are there any other bidders? 'y' or 'n' ")
+    if more_bid == "n":
+        should_continue = False
+        highest_bidder(who_winner_info)
